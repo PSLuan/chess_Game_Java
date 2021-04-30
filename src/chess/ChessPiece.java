b64910970ef7f5d756d08eb72469a2ba92bf8d02
Luan Pereira <luanpereira.main@gmail.com>
2021-04-29 21:56:28 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,6 +12,10 @@ public abstract class ChessPiece extends Piece {
 		super(board);
 		this.color = color;
 	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 
 	public Color getColor() {
 		return color;
